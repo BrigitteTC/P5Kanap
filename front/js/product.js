d8040ec37344eb5706ed4df4751f9d1ec3bdbfca
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-02-23 11:12:38 +0100

    product.js: boucle pour trouver l'Id passé dans la page html et le produit correspondant.

--- front/js/product.js
+++ front/js/product.js
@@ -5,6 +5,9 @@ auteur: BTC
 Script pour mettre à jour la page du canapé sur la page html correspondante
 *******************************************************************/
 
+//server avec la liste des produits
+var serverGET = "http://localhost:3000/api/products";
+
 //REcherche id passé dans la page HTML
 
 //---------------------------------------------------
@@ -27,5 +30,70 @@ function getId() {
 }
 
 //On récupère l'id de la page
-var id = getId;
-console.log(id);
+var productId = getId;
+console.log(productId);
+
+//--------------------------------------------------------------
+// ft getProducts
+// nom: getProduct
+// Paramètres:
+//  server: adresse duserveur avec les données à récupérer
+// retour: data: données récupérées
+//
+// algo:
+//  lit les données du serveur
+// await: attend le retour de la promese
+//-------------------------------------------------------------
+var getProductById = async function (server) {
+  let data; //données récupérées par la ft
+  try {
+    let response = await fetch(server);
+    if (response.ok) {
+      data = await response.json();
+      console.log(data);
+
+      var str = window.location.href;
+      console.log(str);
+      var url = new URL(str);
+      var search_params = new URLSearchParams(url.search);
+      if (search_params.has("id")) {
+        var ProductId = search_params.get("id");
+        console.log(ProductId);
+      }
+
+      //recherche de l'Id
+      let FoundProduct = false; /*init boolean à false Sera true quand on aura trouve*/
+      LoopIndex = 0; /*index de boucle pour parcourir les produits*/
+      while (LoopIndex < data.length && FoundProduct === false) {
+        if (data[LoopIndex]._id === ProductId) {
+          FoundProduct = true;
+        }
+        LoopIndex++;
+      }
+
+      //Affichage du résultat dans le HTML
+
+      // creation des elts du html
+      //Maj titre  id='title'
+      document.getElementById("title").innerHTML = data[LoopIndex].name;
+
+      //Maj image  class= item__img"
+
+      //maj description  id="description"
+
+      //maj options  id="colors"
+
+      //DEBUG: affichage elts
+      console.log(LoopIndex);
+      console.log(data[LoopIndex].name);
+    } else {
+      console.error("Retour du serveur:", response.status);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Appel de la ft pour récupérer les produits
+
+getProductById(serverGET);
