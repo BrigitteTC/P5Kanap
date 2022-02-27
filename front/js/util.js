83c57e59261bf3a6936034b65c0737c04ff493ad
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-02-27 11:22:55 +0100

    util.js: déplacement de la ft getId dans ce module.

--- front/js/util.js
+++ front/js/util.js
@@ -1,9 +1,37 @@
 //-------------------------------------------------
 // Fonctions et variables utiles pour java script
 //---------------------------------------------------
+
+//------------------------------------------------------
+//Variables
+//--------------------------------------------------------
+
 //server avec la liste des produits
-var serverGET = "http://localhost:3000/api/products";
+const serverGET = "http://localhost:3000/api/products";
 
 // choix d'option: msg pour demander à l'uitlisateur de choisir la couleur
 const C_choixOption = "--SVP, choisissez une couleur --";
 const C_undefined = "undefined";
+
+//----------------------------------------
+// Fonctions
+//----------------------------------------
+
+//---------------------------------------------------
+//fonction getId
+// Retourne l'id du produit de la page
+//params entrée: aucun
+//retour: id
+//------------------------------------------------
+
+function getId() {
+  var str = window.location.href;
+  console.log(str);
+  var url = new URL(str);
+  var search_params = new URLSearchParams(url.search);
+  if (search_params.has("id")) {
+    var id = search_params.get("id");
+    console.log(id);
+    return id;
+  }
+}
